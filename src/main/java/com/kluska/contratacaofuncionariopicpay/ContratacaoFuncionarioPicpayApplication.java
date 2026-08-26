8c739c09c3836f35523cf7fb95d42cf4494cef22
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-08-26 15:36:16 -0300

    chore: initial project scaffolding

---
+++ src/main/java/com/kluska/contratacaofuncionariopicpay/ContratacaoFuncionarioPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.kluska.contratacaofuncionariopicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratacaoFuncionarioPicpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContratacaoFuncionarioPicpayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kluska/contratacaofuncionariopicpay/ContratacaoFuncionarioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kluska.contratacaofuncionariopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratacaoFuncionarioPicpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
